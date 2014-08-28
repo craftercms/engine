c36bcd55de262be9e3a8e72c650be18d887a7d5e
Alfonso Vásquez <avasquez@rivetlogic.com>
2014-08-28 11:00:55 -0600

    Fix for CRAFTERCMS-863.

--- src/main/java/org/craftercms/engine/view/UserAgentAwareCrafterPageView.java
+++ src/main/java/org/craftercms/engine/view/UserAgentAwareCrafterPageView.java
@@ -1,11 +1,13 @@
 package org.craftercms.engine.view;
 
-import org.craftercms.engine.mobile.UserAgentTemplateDetector;
-import org.springframework.beans.factory.annotation.Required;
-
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
+
+import org.craftercms.engine.exception.RenderingException;
+import org.craftercms.engine.mobile.UserAgentTemplateDetector;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.web.servlet.View;
 
 public class UserAgentAwareCrafterPageView extends CrafterPageView {
 	
@@ -20,8 +22,15 @@ public class UserAgentAwareCrafterPageView extends CrafterPageView {
 	protected void renderActualView(String pageViewName, Map<String, Object> model, HttpServletRequest request,
                                     HttpServletResponse response) throws Exception {
     	String userAgentSpecificPageViewName = userAgentTemplateDetector.resolveAgentTemplate(request, pageViewName);
+        View actualView = delegatedViewResolver.resolveViewName(userAgentSpecificPageViewName, locale);
+        if (actualView == null) {
+            actualView = delegatedViewResolver.resolveViewName(pageViewName, locale);
+            if (actualView == null) {
+                throw new RenderingException("No view was resolved for page view name '" + pageViewName + "'");
+            }
+        }
 
-        super.renderActualView(userAgentSpecificPageViewName, model, request, response);
+        actualView.render(model, request, response);
 	}
 
 }
