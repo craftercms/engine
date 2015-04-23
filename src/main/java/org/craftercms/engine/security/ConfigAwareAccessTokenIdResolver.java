7bbaed2330f4cc1b21b846187ca04a5fcca19b15
Alfonso Vásquez <avasquez@rivetlogic.com>
2015-04-23 15:24:54 -0600

    Added functionality for using an access token per site.

--- src/main/java/org/craftercms/engine/security/ConfigAwareAccessTokenIdResolver.java
+++ src/main/java/org/craftercms/engine/security/ConfigAwareAccessTokenIdResolver.java
@@ -1,27 +1,36 @@
 package org.craftercms.engine.security;
 
 import org.apache.commons.configuration.HierarchicalConfiguration;
+import org.apache.commons.lang3.StringUtils;
+import org.craftercms.engine.exception.ConfigurationException;
+import org.craftercms.engine.service.context.SiteContext;
 import org.craftercms.engine.util.ConfigUtils;
 import org.craftercms.profile.services.impl.AccessTokenIdResolver;
-import org.craftercms.profile.services.impl.SingleAccessTokenIdResolver;
 
 /**
  * Special {@link AccessTokenIdResolver} used in multi-tenant engine that returns the access token ID from the site
- * configuration, returning a default ID if not found.
+ * configuration, throwing an {@link ConfigurationException} if not found.
  *
  * @author avasquez
  */
-public class ConfigAwareAccessTokenIdResolver extends SingleAccessTokenIdResolver {
+public class ConfigAwareAccessTokenIdResolver implements AccessTokenIdResolver {
 
     public static final String ACCESS_TOKEN_ID_KEY = "profile.api.accessTokenId";
 
     @Override
     public String getAccessTokenId() {
+        String accessTokenId = null;
         HierarchicalConfiguration config = ConfigUtils.getCurrentConfig();
+
         if (config != null) {
-            return config.getString(ACCESS_TOKEN_ID_KEY, accessTokenId);
-        } else {
+            accessTokenId = config.getString(ACCESS_TOKEN_ID_KEY);
+        }
+
+        if (StringUtils.isNotEmpty(accessTokenId)) {
             return accessTokenId;
+        } else {
+            throw new ConfigurationException("Current config for site '" + SiteContext.getCurrent().getSiteName() +
+                                            "' doesn't contain required property " + ACCESS_TOKEN_ID_KEY);
         }
     }
 
