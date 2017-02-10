d7fef2b5a30d91b9184d0fc5bf831ed5a483defe
Alfonso Vásquez <avasquez@rivetlogic.com>
2017-02-10 14:43:11 -0500

    Fix for issue #393

---
+++ src/main/java/org/craftercms/engine/util/predicates/DisabledItemPredicate.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2007-2017 Crafter Software Corporation.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.craftercms.engine.util.predicates;
+
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.craftercms.core.service.Item;
+import org.springframework.beans.factory.annotation.Required;
+
+/**
+ * Predicate used to check if an item is disabled.
+ *
+ * @author avasquez
+ */
+public class DisabledItemPredicate implements Predicate<Item> {
+
+    private static final Log logger = LogFactory.getLog(DisabledItemPredicate.class);
+
+    protected String disabledXPathQuery;
+
+    @Required
+    public void setDisabledXPathQuery(String disabledXPathQuery) {
+        this.disabledXPathQuery = disabledXPathQuery;
+    }
+
+    @Override
+    public boolean evaluate(Item item) {
+        String disabled = item.queryDescriptorValue(disabledXPathQuery);
+        if (StringUtils.isNotEmpty(disabled) && Boolean.parseBoolean(disabled)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+}
