650e3afdc4166e59dd61b71a86300756b56fe809
jdrossl <jdross17@gmail.com>
2017-05-30 14:04:12 -0600

    Updated monitoring controller with classes from commons craftercms/craftercms#853

--- src/main/java/org/craftercms/engine/controller/MonitoringController.java
+++ src/main/java/org/craftercms/engine/controller/MonitoringController.java
@@ -1,43 +1,58 @@
 package org.craftercms.engine.controller;
 
-import java.util.Collections;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
+import org.craftercms.commons.monitoring.MemoryMonitor;
+import org.craftercms.commons.monitoring.StatusMonitor;
+import org.craftercms.commons.monitoring.VersionMonitor;
 import org.craftercms.engine.util.logging.CircularQueueLogAppender;
-import org.springframework.beans.factory.annotation.Required;
-import org.springframework.stereotype.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(MonitoringController.URL_ROOT)
 public class MonitoringController {
 
+    private static final Logger logger = LoggerFactory.getLogger(MonitoringController.class);
+
     public final static String URL_ROOT = "/api/1/monitoring";
+    public final static String MEMORY_TARGET_URL = "/memory";
+    public final static String STATUS_TARGET_URL = "/status";
+    public final static String VERSION_TARGET_URL = "/version";
+    public final static String LOG_TARGET_URL = "/log";
 
-    public static final String ACTION_PATH_VAR = "action";
-
-    private String statusMessage;
-
-    @Required
-    public void setStatusMessage(String statusMessage) {
-        this.statusMessage = statusMessage;
+    @GetMapping(MEMORY_TARGET_URL)
+    public ResponseEntity<List<MemoryMonitor>> getMemoryStats() {
+        return new ResponseEntity<>(MemoryMonitor.getMemoryStats(), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/status", method = RequestMethod.GET)
-    @ResponseBody
-    public Map<String, String> getStatus() {
-        return Collections.singletonMap("status", statusMessage);
+
+    @GetMapping(STATUS_TARGET_URL)
+    public ResponseEntity<StatusMonitor> getCurrentStatus() {
+        return new ResponseEntity<>(StatusMonitor.getCurrentStatus(), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/log", method = RequestMethod.GET)
-    @ResponseBody
-    public List<HashMap<String,Object>> logger(@RequestParam String siteId, @RequestParam long since) {
-        return CircularQueueLogAppender.loggerQueue().getLoggedEvents(siteId,since);
+    @GetMapping(VERSION_TARGET_URL)
+    public ResponseEntity<VersionMonitor> getCurrentVersion() throws Exception {
+        try {
+            return new ResponseEntity<>(VersionMonitor.getVersion(this.getClass()), HttpStatus.OK);
+        } catch (IOException e) {
+            logger.error("Unable to read manifest file", e);
+            throw new Exception("Unable to read manifest file");
+        }
+    }
+
+    @GetMapping(LOG_TARGET_URL)
+    public List<HashMap<String,Object>> getAppLogger(@RequestParam String site, @RequestParam long since) {
+        return CircularQueueLogAppender.loggerQueue().getLoggedEvents(site, since);
     }
 
 }
