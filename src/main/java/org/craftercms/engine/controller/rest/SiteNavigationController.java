86659658a6a6f884ddb4ad465b4994d52f8b826d
jdrossl <jdross17@gmail.com>
2018-04-10 14:38:20 -0600

    Added missing license to new rest controller

--- src/main/java/org/craftercms/engine/controller/rest/SiteNavigationController.java
+++ src/main/java/org/craftercms/engine/controller/rest/SiteNavigationController.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2007-2018 Crafter Software Corporation. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.craftercms.engine.controller.rest;
 
 import java.util.List;
@@ -12,6 +29,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * REST controller to access site navigation services.
+ *
+ * @author joseross
+ */
 @RestController
 @RequestMapping(RestControllerBase.REST_BASE_URI + SiteNavigationController.URL_ROOT)
 public class SiteNavigationController extends RestControllerBase {
