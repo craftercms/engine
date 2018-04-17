d0f3c6d5953a8a3c7d91294f877a00d8662ece49
jdrossl <jdross17@gmail.com>
2018-04-17 08:05:05 -0600

    Added missing license on new controller

--- src/main/java/org/craftercms/engine/controller/rest/SiteUrlController.java
+++ src/main/java/org/craftercms/engine/controller/rest/SiteUrlController.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2007-2018 Crafter Software Corporation. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.craftercms.engine.controller.rest;
 
 import org.craftercms.core.controller.rest.RestControllerBase;
@@ -8,6 +25,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * REST controller to access the URL transformation service.
+ *
+ * @author joseross
+ */
 @RestController
 @RequestMapping(RestControllerBase.REST_BASE_URI + SiteUrlController.URL_ROOT)
 public class SiteUrlController extends RestControllerBase {
