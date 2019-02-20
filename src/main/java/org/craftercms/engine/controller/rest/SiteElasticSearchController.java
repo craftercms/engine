995d8dacec2f7cc63e4e7532134a0478657d678e
jdrossl <jdross17@gmail.com>
2019-02-20 10:56:23 -0600

    Improvements for ElasticSearch: - Removed uses of ElasticSearch internal classes - Removed GET method for search operation

--- src/main/java/org/craftercms/engine/controller/rest/SiteElasticSearchController.java
+++ src/main/java/org/craftercms/engine/controller/rest/SiteElasticSearchController.java
@@ -17,30 +17,16 @@
 
 package org.craftercms.engine.controller.rest;
 
-import java.io.InputStream;
-import java.nio.charset.Charset;
-import java.util.Collections;
+import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.io.IOUtils;
 import org.craftercms.core.controller.rest.RestControllerBase;
 import org.craftercms.engine.search.SiteAwareElasticSearchService;
-import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.xcontent.DeprecationHandler;
-import org.elasticsearch.common.xcontent.NamedXContentRegistry;
-import org.elasticsearch.common.xcontent.XContentFactory;
-import org.elasticsearch.common.xcontent.XContentType;
-import org.elasticsearch.search.SearchModule;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
  * REST controller to expose the ElasticSearch service
@@ -60,18 +46,9 @@ public class SiteElasticSearchController {
         this.elasticSearchService = elasticSearchService;
     }
 
-    @RequestMapping(value = URL_SEARCH, method = {GET, POST})
-    public SearchResponse search(HttpServletRequest request) throws Exception {
-        try(InputStream is = request.getInputStream()) {
-            String json = IOUtils.toString(is, Charset.defaultCharset());
-            SearchModule module = new SearchModule(Settings.EMPTY, false, Collections.emptyList());
-            SearchSourceBuilder builder =
-                SearchSourceBuilder
-                    .fromXContent(XContentFactory.xContent(XContentType.JSON)
-                        .createParser(new NamedXContentRegistry(module.getNamedXContents()),
-                            DeprecationHandler.THROW_UNSUPPORTED_OPERATION, json));
-            return elasticSearchService.search(new SearchRequest().source(builder));
-        }
+    @PostMapping(URL_SEARCH)
+    public SearchResponse search(@RequestBody Map<String, Object> request) {
+        return elasticSearchService.search(request);
     }
 
 }
