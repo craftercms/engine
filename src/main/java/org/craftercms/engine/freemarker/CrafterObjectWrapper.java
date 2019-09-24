2d4980d17ce71d20cff26a875705cf968ba7c073
jdrossl <jdross17@gmail.com>
2019-09-24 10:19:26 -0600

    Fix warnings during build

--- src/main/java/org/craftercms/engine/freemarker/CrafterObjectWrapper.java
+++ src/main/java/org/craftercms/engine/freemarker/CrafterObjectWrapper.java
@@ -31,6 +31,7 @@ import org.craftercms.engine.model.Dom4jNodeModel;
  *
  * @author Alfonso Vásquez
  */
+@SuppressWarnings("deprecation")
 public class CrafterObjectWrapper extends DefaultObjectWrapper {
 
     @Override
