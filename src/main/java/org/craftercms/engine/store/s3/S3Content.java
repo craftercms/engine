24416b38b28da23ebc4d9eb2ea776ddbc660a424
Alfonso Vasquez <alfonso.vasquez@craftersoftware.com>
2019-10-03 11:56:24 -0400

    Adde missing Javadoc and copyrights.

--- src/main/java/org/craftercms/engine/store/s3/S3Content.java
+++ src/main/java/org/craftercms/engine/store/s3/S3Content.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2007-2019 Crafter Software Corporation. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.craftercms.engine.store.s3;
 
 import com.amazonaws.services.s3.model.S3Object;
@@ -8,6 +24,13 @@ import org.craftercms.core.service.Content;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
+/**
+ * Represents the content of an S3 object.
+ *
+ * @author jross
+ * @author avasquez
+ * @since 3.1.4
+ */
 public class S3Content implements Content {
 
     /**
