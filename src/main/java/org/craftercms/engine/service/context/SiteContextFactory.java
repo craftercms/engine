fc2a8635b528598143420c06fcb1bc1bc4815e10
Jose Ross <jdross17@gmail.com>
2020-04-24 15:53:34 -0600

    Add a default list of allowed beans

--- src/main/java/org/craftercms/engine/service/context/SiteContextFactory.java
+++ src/main/java/org/craftercms/engine/service/context/SiteContextFactory.java
@@ -111,6 +111,7 @@ public class SiteContextFactory implements ApplicationContextAware, ServletConte
     protected long initTimeout;
     protected EncryptionAwareConfigurationReader configurationReader;
     protected boolean disableVariableRestrictions;
+    protected List<String> defaultPublicBeans;
 
     public SiteContextFactory() {
         siteNameMacroName = DEFAULT_SITE_NAME_MACRO_NAME;
@@ -119,6 +120,7 @@ public class SiteContextFactory implements ApplicationContextAware, ServletConte
         maxAllowedItemsInCache = Context.DEFAULT_MAX_ALLOWED_ITEMS_IN_CACHE;
         ignoreHiddenFiles = Context.DEFAULT_IGNORE_HIDDEN_FILES;
         initTimeout = DEFAULT_INIT_TIMEOUT;
+        defaultPublicBeans = Collections.emptyList();
     }
 
     @Override
@@ -269,6 +271,10 @@ public class SiteContextFactory implements ApplicationContextAware, ServletConte
         this.disableVariableRestrictions = disableVariableRestrictions;
     }
 
+    public void setDefaultPublicBeans(List<String> defaultPublicBeans) {
+        this.defaultPublicBeans = defaultPublicBeans;
+    }
+
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         this.globalApplicationContext = applicationContext;
@@ -410,7 +416,7 @@ public class SiteContextFactory implements ApplicationContextAware, ServletConte
                 if (disableVariableRestrictions) {
                     appContext = new GenericApplicationContext(globalApplicationContext);
                 } else {
-                    appContext = new RestrictedApplicationContext(globalApplicationContext);
+                    appContext = new RestrictedApplicationContext(globalApplicationContext, defaultPublicBeans);
                 }
                 appContext.setClassLoader(classLoader);
 
