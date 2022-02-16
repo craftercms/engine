ad1bf803d9fec75f9e23978a727fcf9966234cc6
Sumer Jabri <sumer.jabri@craftercms.org>
2022-02-16 10:05:24 -0500

    Updated copyright year.

--- src/main/java/org/craftercms/engine/util/CacheUtils.java
+++ src/main/java/org/craftercms/engine/util/CacheUtils.java
@@ -2,7 +2,7 @@ package org.craftercms.engine.util;
 
 import org.craftercms.core.service.ContentStoreService;
 /*
- * Copyright (C) 2007-2020 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
