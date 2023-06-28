3d2384f482bbd52b856803c712416b7cd0140881
Jonathan Mendez <jonnathan.mendez@gmail.com>
2023-06-27 19:19:27 -0600

    Site context rebuild_all API

--- src/main/java/org/craftercms/engine/service/context/SiteContextManager.java
+++ src/main/java/org/craftercms/engine/service/context/SiteContextManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2023 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
@@ -527,4 +527,12 @@ public class SiteContextManager implements ApplicationContextAware, DisposableBe
         }
     }
 
+    /**
+     * Rebuild all site contexts currently found in the registry
+     */
+    public void startRebuildAll() {
+        this.contextRegistry.values().forEach(siteContext ->
+                startContextRebuild(siteContext.getSiteName(), siteContext.isFallback())
+        );
+    }
 }
