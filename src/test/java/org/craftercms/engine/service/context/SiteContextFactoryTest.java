1ac9e678247de4fe12ba5aace34735aa5bca2d13
Phil Nguyen <nquangphuong@gmail.com>
2024-04-30 14:17:49 +0700

    Update test case

--- src/test/java/org/craftercms/engine/service/context/SiteContextFactoryTest.java
+++ src/test/java/org/craftercms/engine/service/context/SiteContextFactoryTest.java
@@ -15,16 +15,26 @@
  */
 package org.craftercms.engine.service.context;
 
+import org.craftercms.commons.config.EncryptionAwareConfigurationReader;
 import org.craftercms.core.service.ContentStoreService;
+import org.craftercms.core.url.UrlTransformationEngine;
+import org.craftercms.core.util.cache.CacheTemplate;
+import org.craftercms.engine.cache.SiteCacheWarmer;
+import org.craftercms.engine.graphql.GraphQLFactory;
 import org.craftercms.engine.macro.MacroResolver;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfig;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.Executor;
 
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
@@ -34,24 +44,35 @@ import static org.mockito.Mockito.when;
 public class SiteContextFactoryTest {
 
     private static final String SITE_NAME_VARIABLE = "siteName";
-    private static final String SITE_ID_VARIABLE = "siteId";
     private static final String SITE_NAME = "my-test-site";
 
     @Mock
     MacroResolver macroResolver;
-    @Mock
+    @Spy
     ContentStoreService contentStoreService;
     @Mock
     ObjectFactory<FreeMarkerConfig> freeMarkerConfigurationFactory;
-    @InjectMocks
+    @Mock
+    UrlTransformationEngine urlTransformationEngine;
+    @Mock
+    CacheTemplate cacheTemplate;
+    @Mock
+    Executor jobThreadPoolExecutor;
+    @Mock
+    GraphQLFactory graphQLFactory;
+    @Mock
+    SiteCacheWarmer cacheWarmer;
+    @Mock
+    EncryptionAwareConfigurationReader configurationReader;
     SiteContextFactory siteContextFactory;
 
     @Before
     public void setUp() throws Exception {
-        siteContextFactory.setConfigPaths(new String[]{});
-        siteContextFactory.setApplicationContextPaths(new String[]{});
-        siteContextFactory.setUrlRewriteConfPaths(new String[]{});
-        siteContextFactory.setProxyConfigPaths(new String[]{});
+        siteContextFactory = new SiteContextFactory("", "", "", "",
+                "", "", "", new String[]{}, new String[]{}, new String[]{}, new String[]{},
+                "", new HashMap<>(), freeMarkerConfigurationFactory, urlTransformationEngine, contentStoreService, cacheTemplate,
+                macroResolver, new ArrayList<>(), jobThreadPoolExecutor, graphQLFactory, false,
+                cacheWarmer, configurationReader);
         siteContextFactory.setTranslationConfigPaths(new String[]{});
         when(macroResolver.resolveMacros(any(), any())).thenReturn("");
     }
