b75edac39a61fdfee21463fee25ca42a3d66426b
Jonathan Méndez <jonnathan.mendez@gmail.com>
2024-07-16 12:56:15 -0600

    Accept either String (comma separated list) or String arrays as roles attribute for profile (#743)

--- src/main/java/org/craftercms/engine/util/spring/security/targeting/TargetingPreAuthenticatedFilter.java
+++ src/main/java/org/craftercms/engine/util/spring/security/targeting/TargetingPreAuthenticatedFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2024 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
@@ -16,17 +16,17 @@
 
 package org.craftercms.engine.util.spring.security.targeting;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.bson.types.ObjectId;
 import org.craftercms.engine.controller.rest.preview.ProfileRestController;
 import org.craftercms.engine.util.spring.security.ConfigAwarePreAuthenticationFilter;
 import org.craftercms.profile.api.Profile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.apache.commons.collections4.MapUtils.isNotEmpty;
 
@@ -52,7 +52,7 @@ public class TargetingPreAuthenticatedFilter extends ConfigAwarePreAuthenticatio
     protected Object getPreAuthenticatedPrincipal(final HttpServletRequest request) {
         HttpSession session = request.getSession();
         if (session != null) {
-            Map<String, String> attributes = (Map<String, String>)
+            Map<String, Object> attributes = (Map<String, Object>)
                 session.getAttribute(ProfileRestController.PROFILE_SESSION_ATTRIBUTE);
 
             if (isNotEmpty(attributes)) {
@@ -61,16 +61,21 @@ public class TargetingPreAuthenticatedFilter extends ConfigAwarePreAuthenticatio
                 }
 
                 Profile profile = new Profile();
-                profile.setId(new ObjectId(attributes.get("id")));
+                profile.setId(new ObjectId((String) attributes.get("id")));
                 profile.setUsername("preview");
                 profile.setEnabled(true);
                 profile.setCreatedOn(new Date());
                 profile.setLastModified(new Date());
                 profile.setTenant("preview");
 
-                String rolesStr = attributes.get("roles");
-                if (rolesStr != null) {
-                    String[] roles = rolesStr.split(",");
+                Object rolesAttr = attributes.get("roles");
+                String[] roles = null;
+                if (rolesAttr instanceof String[]) {
+                    roles = (String[]) rolesAttr;
+                } else if (rolesAttr instanceof String) {
+                    roles = ((String) rolesAttr).split(",");
+                }
+                if (roles != null) {
                     profile.getRoles().addAll(Arrays.asList(roles));
                 }
 
