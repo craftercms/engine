a8f2412d3ff1e3ae534ab3ed1208f7d515b630e0
Phil Nguyen <nquangphuong@gmail.com>
2025-01-28 15:21:02 +0700

    Skip preview token validation in the case of pre-flight OPTIONS requests if CORS is enabled

--- src/main/java/org/craftercms/engine/util/spring/security/preview/ConfigAwarePreviewAccessTokenFilter.java
+++ src/main/java/org/craftercms/engine/util/spring/security/preview/ConfigAwarePreviewAccessTokenFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2024 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2025 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
@@ -20,16 +20,21 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.collections4.CollectionUtils;
 import org.craftercms.commons.crypto.CryptoException;
 import org.craftercms.commons.crypto.TextEncryptor;
 import org.craftercms.commons.http.HttpUtils;
 import org.craftercms.engine.exception.PreviewAccessException;
 import org.craftercms.engine.service.context.SiteContext;
+import org.craftercms.engine.util.spring.cors.SiteAwareCorsConfigurationSource;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.filter.GenericFilterBean;
 
 import java.beans.ConstructorProperties;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -39,15 +44,18 @@ import static org.apache.commons.lang3.StringUtils.isEmpty;
 /**
  * Filter that checks if the user is authorized to preview the site.
  */
-public class PreviewAccessTokenFilter extends GenericFilterBean {
+public class ConfigAwarePreviewAccessTokenFilter extends GenericFilterBean {
     private final static String PREVIEW_SITE_TOKEN_NAME = "crafterPreview";
     private final static String PREVIEW_SITE_TOKEN_HEADER_NAME = "X-Crafter-Preview";
 
     private final TextEncryptor textEncryptor;
+	private final SiteAwareCorsConfigurationSource corsConfigSource;
 
-    @ConstructorProperties({"textEncryptor"})
-    public PreviewAccessTokenFilter(final TextEncryptor textEncryptor) {
+    @ConstructorProperties({"textEncryptor", "corsConfigSource"})
+    public ConfigAwarePreviewAccessTokenFilter(final TextEncryptor textEncryptor,
+											   final SiteAwareCorsConfigurationSource corsConfigSource) {
         this.textEncryptor = textEncryptor;
+		this.corsConfigSource = corsConfigSource;
     }
 
     @Override
@@ -58,6 +66,11 @@ public class PreviewAccessTokenFilter extends GenericFilterBean {
             chain.doFilter(request, response);
             return;
         }
+
+		if (skipTokenValidation(httpServletRequest)) {
+			chain.doFilter(request, response);
+			return;
+		}
 
         String previewToken = httpServletRequest.getHeader(PREVIEW_SITE_TOKEN_HEADER_NAME);
         if (isEmpty(previewToken)) {
@@ -119,4 +132,43 @@ public class PreviewAccessTokenFilter extends GenericFilterBean {
             throw new PreviewAccessException(HttpStatus.UNAUTHORIZED, message);
         }
     }
+
+	/**
+	 * Determines whether token validation should be skipped for the given HTTP request.
+	 * This is typically used for preflight OPTIONS requests in CORS (Cross-Origin Resource Sharing)
+	 * scenarios, where token validation is not required.
+	 *
+	 * @param request The HTTP request to evaluate.
+	 * @return {@code true} if token validation should be skipped, {@code false} otherwise.
+	 */
+	private boolean skipTokenValidation(HttpServletRequest request) {
+		if (!CorsUtils.isCorsRequest(request) || !CorsUtils.isPreFlightRequest(request)) {
+			return false;
+		}
+
+		return corsAllowedOrigin(request);
+	}
+
+	/**
+	 * Checks if the `Origin` header of the given HTTP request is allowed based on the
+	 * configured CORS (Cross-Origin Resource Sharing) origin patterns. If no CORS configuration
+	 * is found or if the `Origin` header is missing or empty, the origin is not allowed.
+	 *
+	 * @param request The HTTP request to check for allowed CORS origin.
+	 * @return {@code true} if the `Origin` header is allowed according to the CORS configuration;
+	 *         {@code false} otherwise.
+	 */
+	private boolean corsAllowedOrigin(HttpServletRequest request) {
+		CorsConfiguration corsConfiguration = corsConfigSource.getCorsConfiguration(request);
+		if (corsConfiguration == null) {
+			return false;
+		}
+
+		String origin = request.getHeader("Origin");
+		if (isEmpty(origin)) {
+			return false;
+		}
+
+		return corsConfiguration.checkOrigin(origin) != null;
+	}
 }
